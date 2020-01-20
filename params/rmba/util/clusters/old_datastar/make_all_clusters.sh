fbd007aced44e6bb503c5fd70f7afc72edc18fa1
Dan Korytov <dkorytov@gmail.com>
2020-01-20 16:43:49 -0600

    new roomba param files

---
+++ params/rmba/util/clusters/old_datastar/make_all_clusters.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+./core_fit params/cfn/simet/clusters/m200c.param
+./core_fit params/cfn/simet/clusters/m200c.peak.param
+./core_fit params/cfn/simet/clusters/m200m.param
+./core_fit params/cfn/simet/clusters/m200m.peak.param
