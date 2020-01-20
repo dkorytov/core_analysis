8c535a2abf2cc8094a9a0a92c6e04323e8096db2
Dan Korytov <dkorytov@gmail.com>
2020-01-20 16:45:34 -0600

    updated for roomba

--- plot_core_properties.py
+++ plot_core_properties.py
@@ -17,7 +17,6 @@ import h5py
 
 def load_prop(fname, var, cat):
     cat[var] = dtk.gio_read(fname, var)
-
     
 def load_cores(fname, step=None, quick=False):
     cat = {}
@@ -114,8 +113,6 @@ def get_full_core_catalog(core_fname_pattern, steps):
         final_cat[key]=np.concatenate(column)
     return final_cat
     
-
-            
 def plot_core_trajectory(core_fname_pattern, steps):
     cat = get_full_core_catalog(core_fname_pattern, steps)
 
@@ -125,13 +122,13 @@ def plot_core_trajectory(core_fname_pattern, steps):
     # sort ids by infall tree node mass
     core_masses = cat['infall_tree_node_mass'][slct]
     srt = np.argsort(core_masses)[::-1]
+    srt = np.argsort(srt)
     core_tags = cat['core_tag'][slct][srt]
     core_masses = core_masses[srt]
 
     for core_tag, core_mass in zip(core_tags, core_masses):
         slct = cat['core_tag']==core_tag
         srt = np.argsort(cat['step'][slct])
-
         x = cat['x'][slct][srt]
         y = cat['y'][slct][srt]
         z = cat['z'][slct][srt]
@@ -141,23 +138,26 @@ def plot_core_trajectory(core_fname_pattern, steps):
         step = cat['step'][slct][srt]
         radius = cat['radius'][slct][srt]
         mass = cat['infall_tree_node_mass'][slct][srt]
-        central = cat['central']
+        central = cat['central'][slct][srt] == 1
         print(list(zip(x,y)))
         print(cat['step'][slct][srt])
         step_mid = dtk.bins_avg(step)
         central_step = np.diff(central)
         
         fig, axs = plt.subplots(2, 3, figsize=(15, 8))
-        fig.suptitle('TreeNodeMass={:.2e}'.format(core_mass))
-        axs[0][0].plot(x-x[-1], y-y[-1], '-o', label='Path')
-        axs[0][0].plot(x[-1]-x[-1], y[-1]-y[-1], 'o', ms=20, mfc='none', label='Last Pos')
+        fig.suptitle('TreeNodeMass={:.2e}\nCore Tag:{}'.format(core_mass, core_tag))
+        axs[0][0].plot(x-x[-1], y-y[-1], '-o', mfc='none', label='Path')
+        axs[0][0].plot(x[-1]-x[-1], y[-1]-y[-1], 'o', ms=20, mfc='none', label='Last Pos', color='tab:orange')
+        axs[0][0].plot(x[central]-x[-1], y[central]-y[-1], 'o', label='Central', color='tab:blue')
+
         axs[0][0].legend(framealpha=0.0)
         axs[0][0].axis('equal')
         axs[0][0].set_xlabel('x')
         axs[0][0].set_ylabel('y')
         
-        axs[0][1].plot(z-z[-1], y-y[-1], '-o', label='Path')
+        axs[0][1].plot(z-z[-1], y-y[-1], '-o', mfc='none', label='Path')
         axs[0][1].plot(z[-1]-z[-1], y[-1]-y[-1], 'o', ms=20, mfc='none', label='Last Pos')
+        axs[0][1].plot(z[central]-z[-1], y[central]-y[-1], 'o', label='Central', color='tab:blue')
         axs[0][1].axis('equal')
         axs[0][1].set_xlabel('z')
         axs[0][1].set_ylabel('y')
@@ -187,35 +187,38 @@ def plot_core_trajectory(core_fname_pattern, steps):
         axs[1][2].set_xlabel('Mid Step')
         axs[1][2].set_ylabel('Angle between displ. and vel. [deg.]')
 
-        axs[1][0].plot(step, mass, '-o')
+        axs[1][0].plot(step, mass, '-o', mfc='none')
         axs[1][0].fill_between(step, np.min(mass), mass, alpha=0.3)
         axs[1][0].plot(step[-1], mass[-1], 'o', ms=20, mfc='none')
+        axs[1][0].plot(step[central], mass[central], 'o', color='tab:blue')
         axs[1][0].set_xlabel('Step')
         axs[1][0].set_ylabel('Infall Mass [Msun/h]')
         axs[1][0].set_yscale('log')
 
-        axs[1][1].plot(step, radius*1000.0, '-o')
+        axs[1][1].plot(step, radius*1000.0, '-o', mfc='none')
         axs[1][1].fill_between(step, 0, radius*1000.0, alpha=0.3)
         axs[1][1].plot(step[-1], radius[-1]*1000.0, 'o', ms=20, mfc='none')
+        axs[1][1].plot(step[central], radius[central]*1000.0, 'o', color='tab:blue')
         axs[1][1].set_xlabel('Step')
         axs[1][1].set_ylabel('Core Radius [Kpc/h]')
+        axs[1][1].set_ylim([0, 200])
         # axs[1][1].set_yscale('log')
         plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-        plt.show()
-        # dtk.save_figs('figs/'+__file__+'/individual_cores_massive_satellite/')
+        # plt.show()
+        dtk.save_figs('figs/'+__file__+'/individual_cores_random_rev4070_v2/')
         plt.close()
         
 
 
 
 if __name__ == "__main__":
-    steps=[499, 487, 475, 464, 453, 442, 432, 421, 411, 401, 392, 382, 373, 365, 355, 347, 338, 331, 323, 315, 307, 300, 293, 286, 279, 272, 266, 259, 253, 247, 241, 235, 230, 224, 219, 213, 208, 203, 198, 194, 189, 184, 180, 176, 171, 163, 159, 155, 151, 148, 144, 141, 137, 134, 131, 127, 124, 121, 119, 116, 113, 110, 107, 105, 102, 100]
+    steps=[499, 487, 475, 464, 453, 442, 432, 421, 411, 401, 392, 382, 373, 365, 355, 347, 338, 331, 323, 315, 307, 300, 293, 286, 279, 272, 266, 259, 253, 247, 241, 235, 230, 224, 219, 213, 208, 203, 198, 194, 189, 184, 180, 176, 171, 163, 159, 155, 151, 148, 144, 141, 137, 134, 131, 127, 124, 121, 119, 116, 113, 110, 107, 105, 102, 100, 97, 95, 92, 90, 88, 84, 81, 79, 77, 76, 74, 72, 70, 68, 67, 65, 63, 62, 60, 59, 57, 56, 54, 53, 52, 50, 49, 48, 46, 45, 44, 43]
     # steps=[307, 300, 293, 286, 279, 272, 266, 259, 253, 247, 241, 235, 230, 224, 219, 213, 208, 203, 198, 194, 189, 184, 180, 176, 171, 163, 159, 155, 151, 148, 144, 141, 137, 134, 131, 127, 124, 121, 119, 116, 113, 110, 107, 105, 102, 100]
     # steps=[307, 300, 293, 286, 279, 272, 266, 259, 253, 247, 241, 235, 230, 224, 219, 213, 208]
     # steps=[307, 300, 293, 286, 279, 272, 266, 259]
-    core_fname_pattern = '/data/a/cpac/dkorytov/data/AlphaQ/core_catalog9_updated_centrals/$step$.coreproperties'
-    plot_core_properties(core_fname_pattern.replace('$step$', str(499)))
+    core_fname_pattern = '/data/a/cpac/dkorytov/data/AlphaQ/core_catalog10_rev4070/$step$.coreproperties'
+    # plot_core_properties(core_fname_pattern.replace('$step$', str(487)))
     # dtk.save_figs('figs/'+__file__+'/')
     plt.show()
-    # plot_core_trajectory(core_fname_pattern, steps)
+    plot_core_trajectory(core_fname_pattern, steps)
 
