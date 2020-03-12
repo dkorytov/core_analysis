533d77c65ac14935d42925a2c5827f22b1a23c59
Dan Korytov <dkorytov@gmail.com>
2020-03-12 15:14:05 -0500

    added print statements

--- run_any_make_set.sh
+++ run_any_make_set.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 make_scirpt=$1
-
-
+unset DISPLAY
+echo "Running set with 1 level zoom"
 echo $make_scirpt
+echo ""
+date
+echo ""
 
 ./run_any_make.sh ./core_fit $make_scirpt
-./run_any_make.sh ./calc_likehood_bounds.py $make_scirpt
-./run_any_make.sh ./make_zoom_make_scirpt.py $make_scirpt
-
+./run_any_make.sh ./calc_likelihood_bounds.py $make_scirpt
+./run_any_make.sh ./make_zoom_param.py $make_scirpt
+echo ""
+echo "Phase 1 Done:"
+date
+echo ""
 ./run_any_make.sh ./core_fit $make_scirpt zoom
-./run_any_make.sh ./calc_likehood_bounds.py $make_scirpt zoom
-./run_any_make.sh ./make_zoom_make_scirpt.py $make_scirpt zoom
+./run_any_make.sh ./calc_likelihood_bounds.py $make_scirpt zoom
+./run_any_make.sh ./make_zoom_param.py $make_scirpt zoom
+echo ""
+echo "All done"
+date
+echo ""
